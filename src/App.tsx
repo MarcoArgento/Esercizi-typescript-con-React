60466ea768c66a45acbfe9fc91a70a5df79826d4
Marco Argento <silvermarco96@gmail.com>
2024-02-14 18:22:19 +0100

    Esercizi svolti

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  //let state: any = "START";
+  let [value, setValue] = useState<any>("START");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          setValue((value = Math.random()));
+        }}
+      >
+        {value}
+      </button>
     </div>
   );
 }
